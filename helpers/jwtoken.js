e937b738eb734b53b1ff43f723e7b5eb0056d209
Riki Rifaldi <rkrfld@gmail.com>
2022-01-27 23:13:54 +0700

    test env hardcode

--- helpers/jwtoken.js
+++ helpers/jwtoken.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken')
-const key = process.env.SECRET_KEY
+// const key = process.env.SECRET_KEY
+const key = 'anotherbitofdust'
 const signToken = (payload) => {
+  console.log(key);
   return jwt.sign(payload, key)
 };
 const verifyToken = (token) => {
+  console.log(key);
   return jwt.verify(token, key)
 };
 
